377725e2149afa7f3a553843b9884a81d76a5835
Raffael Barbosa Martins <raffael.martins@receita.pb.gov.br>
2018-11-21 15:22:08 -0300

    Criando a nossa tela

--- pokedex/src/app/views/list/list.component.ts
+++ pokedex/src/app/views/list/list.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  pokeList = [
+    { name: 'Bulbassaur', number: 1 },
+    { name: 'Charmander', number: 4 },
+    { name: 'Squirtle', number: 7 },
+    { name: 'Pikachu', number: 25 }
+  ]
+
   constructor() { }
 
   ngOnInit() {
