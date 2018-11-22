5d55a349f68f4d8439a34798364a161a9a438f9a
Raffael Barbosa Martins <raffael.martins@receita.pb.gov.br>
2018-11-22 10:03:04 -0300

    Importa HttClientModule

--- meu-app/src/app/app.module.ts
+++ meu-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
 import { MeuComponente2Component } from './meu-componente2/meu-componente2.component';
@@ -12,7 +12,8 @@ import { MeuComponente2Component } from './meu-componente2/meu-componente2.compo
     MeuComponente2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
